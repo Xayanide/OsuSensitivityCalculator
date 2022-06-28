114f5e36dc78f9460acf04e713192a59c637e30d
Xayanide <chris.balili.91@gmail.com>
2022-06-28 16:29:23 +0800

    refactor: rewrite attempt

--- script.js
+++ script.js
@@ -1,71 +1,80 @@
-$(document).ready(function() {
+let windowsWidth = 800;
+let gameWidth = 800;
+let windowsHeight = 600;
+let gameHeight = 600;
 
-    $('#ppi').change(function() {
-        ppi = $(this).val();
-        calculateSens();
+function getPlayArea() {
+    if ((windowsWidth / windowsHeight) >= (4.0 / 3)) {
+        gameHeight = windowsHeight;
+        gameWidth = windowsHeight * (4.0 / 3);
+    } else {
+        gameWidth = windowsWidth;
+        gameHeight = windowsWidth / (4 / 3);
+    }
+}
+
+function getWindowsMultiplier(sensitivity) {
+    if (sensitivity >= 1 && sensitivity <= 11) {
+        const list = [0.00625, 0.0125, 0.25, 0.5, 0.75, 1, 1.5, 2, 2.5, 3, 3.5];
+        return list[sensitivity - 1];
+    } else {
+        return 0;
+    }
+}
+
+let ppi = 800;
+let windowsSensitivity = 6;
+let gameSensitivity = 1;
+let effectivePPI;
+let effectiveWidth;
+let effectiveHeight;
+function calculateSensitivity() {
+    getPlayArea();
+    effectivePPI = ppi * getWindowsMultiplier(windowsSensitivity) * gameSensitivity;
+    document.getElementById('playPPI').innerHTML = effectivePPI;
+    document.getElementById('playResolution').innerHTML = `${gameWidth}x${gameHeight}`;
+    effectiveWidth = Math.round((gameWidth / effectivePPI) * 100) / 100;
+    effectiveHeight = Math.round((gameHeight / effectivePPI) * 100) / 100;
+    document.getElementById('playAreaIN').innerHTML = document.querySelector('#roundPlayAreas').checked ? `${Math.round(effectiveWidth)}" x ${Math.round(effectiveHeight)}"` : `${effectiveWidth}" x ${effectiveHeight}"`;
+    document.getElementById('playAreaCM').innerHTML = document.querySelector('#roundPlayAreas').checked ? `${Math.round(effectiveWidth * 2.54)}cm x ${Math.round(effectiveHeight * 2.54)}cm` : `${effectiveWidth * 2.54}cm x ${effectiveHeight * 2.54}cm`;
+    document.getElementById('playAreaMM').innerHTML = document.querySelector('#roundPlayAreas').checked ? `${Math.round(effectiveWidth * 25.4)}mm x ${Math.round(effectiveHeight * 25.4)}mm` : `${effectiveWidth * 25.4}mm x ${effectiveHeight * 25.4}mm`;
+}
+
+window.onload = () => {
+
+    document.getElementById('ppiInput').addEventListener('change', function() {
+        ppi = document.getElementById('ppiInput').value;
+        calculateSensitivity();
     });
 
-    $('#wintext').change(function() {
-        winsens = $(this).val();
-        calculateSens();
+    document.getElementById('windowsNumberInput').addEventListener('change', function() {
+        windowsSensitivity = document.getElementById('windowsNumberInput').value;
+        calculateSensitivity();
     });
 
-    $('#winsens').change(function() {
-        winsens = $(this).val();
-        calculateSens();
+    document.getElementById('windowsRangeInput').addEventListener('change', function() {
+        windowsSensitivity = document.getElementById('windowsRangeInput').value;
+        calculateSensitivity();
     });
 
-    $('#osusens').change(function() {
-        osusens = $(this).val();
-        calculateSens();
+    document.getElementById('gameNumberInput').addEventListener('change', function() {
+        gameSensitivity = document.getElementById('gameNumberInput').value;
+        calculateSensitivity();
     });
 
-    $('#width').change(function() {
-        winWidth = $(this).val();
-        calculateSens();
+    document.getElementById('windowsWidthInput').addEventListener('change', function() {
+        windowsWidth = document.getElementById('windowsWidthInput').value;
+        calculateSensitivity();
     });
 
-    $('#height').change(function() {
-        winHeight = $(this).val();
-        calculateSens();
+    document.getElementById('windowsHeightInput').addEventListener('change', function() {
+        windowsHeight = document.getElementById('windowsHeightInput').value;
+        calculateSensitivity();
     });
 
-    var getPlayArea = function() {
-        if ((winWidth / winHeight) >= (4.0 / 3)) {
-            playHeight = winHeight;
-            playWidth = winHeight * (4.0 / 3);
-        } else {
-            playWidth = winWidth;
-            playHeight = winWidth / (4 / 3);
-        }
-    };
+    document.getElementById('roundPlayAreas').addEventListener('change', function() {
+        calculateSensitivity();
+    });
 
-    var getWinMulti = function(sens) {
-        if (sens >= 1 && sens <= 11) {
-            var list = [0.00625, 0.0125, 0.25, 0.5, 0.75, 1, 1.5, 2, 2.5, 3, 3.5];
-            return list[sens - 1];
-        } else {
-            return 0;
-        }
-    };
-    var calculateSens = function() {
-        getPlayArea();
-        effectivePPI = ppi * getWinMulti(winsens) * osusens;
-        $('#result').text(effectivePPI);
-        $('#playres').text(playWidth + 'x' + playHeight);
-        effwidth = Math.round((playWidth / effectivePPI) * 100) / 100;
-        effHeight = Math.round((playHeight / effectivePPI) * 100) / 100;
-        $('#playarea').text(effwidth + '" x ' + effHeight + '"');
-        $('#playareacm').text(effwidth * 2.54 + 'cm x ' + effHeight * 2.54 + 'cm');
-    };
-    var effwidth;
-    var effheight;
-    var ppi = 800;
-    var winsens = 6;
-    var osusens = 1;
-    var winWidth = 800;
-    var winHeight = 600;
-    var playWidth = 800;
-    var playHeight = 600;
-    calculateSens();
-});
+    calculateSensitivity();
+};
