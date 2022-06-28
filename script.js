b4f89eefc2bc3832b1e19fb4db13af3c9539210c
Xayanide <chris.balili.91@gmail.com>
2022-06-28 19:52:57 +0800

    refactor: use anonymous functions for now instead

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let osuGameWidth = 800;
 let windowsHeight = 600;
 let osuGameHeight = 600;
 
+/*
 function getPlayArea() {
     if ((windowsWidth / windowsHeight) >= (4.0 / 3)) {
         osuGameHeight = windowsHeight;
@@ -12,7 +13,28 @@ function getPlayArea() {
         osuGameHeight = windowsWidth / (4 / 3);
     }
 }
+*/
 
+const getPlayArea = function() {
+    if ((windowsWidth / windowsHeight) >= (4.0 / 3)) {
+        osuGameHeight = windowsHeight;
+        osuGameWidth = windowsHeight * (4.0 / 3);
+    } else {
+        osuGameWidth = windowsWidth;
+        osuGameHeight = windowsWidth / (4 / 3);
+    }
+};
+
+const getWindowsMultiplier = function(sensitivity) {
+    if (sensitivity >= 1 && sensitivity <= 11) {
+        const list = [0.00625, 0.0125, 0.25, 0.5, 0.75, 1, 1.5, 2, 2.5, 3, 3.5];
+        return list[sensitivity - 1];
+    } else {
+        return 0;
+    }
+};
+
+/*
 function getWindowsMultiplier(sensitivity) {
     if (sensitivity >= 1 && sensitivity <= 11) {
         const list = [0.00625, 0.0125, 0.25, 0.5, 0.75, 1, 1.5, 2, 2.5, 3, 3.5];
@@ -21,6 +43,7 @@ function getWindowsMultiplier(sensitivity) {
         return 0;
     }
 }
+*/
 
 let ppi = 800;
 let windowsSensitivity = 6;
@@ -29,6 +52,19 @@ let effectivePPI;
 let effectiveWidth;
 let effectiveHeight;
 
+const calculateSensitivity = function() {
+    getPlayArea();
+    effectivePPI = ppi * getWindowsMultiplier(windowsSensitivity) * osuGameSensitivity;
+    document.getElementById('play-ppi').innerHTML = effectivePPI;
+    document.getElementById('play-resolution').innerHTML = `${osuGameWidth}x${osuGameHeight}`;
+    effectiveWidth = Math.round((osuGameWidth / effectivePPI) * 100) / 100;
+    effectiveHeight = Math.round((osuGameHeight / effectivePPI) * 100) / 100;
+    document.getElementById('playarea-in').innerHTML = document.querySelector('#round-playarea').checked ? `${Math.round(effectiveWidth)}" x ${Math.round(effectiveHeight)}"` : `${effectiveWidth}" x ${effectiveHeight}"`;
+    document.getElementById('playarea-cm').innerHTML = document.querySelector('#round-playarea').checked ? `${Math.round(effectiveWidth * 2.54)}cm x ${Math.round(effectiveHeight * 2.54)}cm` : `${effectiveWidth * 2.54}cm x ${effectiveHeight * 2.54}cm`;
+    document.getElementById('playarea-mm').innerHTML = document.querySelector('#round-playarea').checked ? `${Math.round(effectiveWidth * 25.4)}mm x ${Math.round(effectiveHeight * 25.4)}mm` : `${effectiveWidth * 25.4}mm x ${effectiveHeight * 25.4}mm`;
+};
+
+/*
 function calculateSensitivity() {
     getPlayArea();
     effectivePPI = ppi * getWindowsMultiplier(windowsSensitivity) * osuGameSensitivity;
@@ -40,6 +76,7 @@ function calculateSensitivity() {
     document.getElementById('playarea-cm').innerHTML = document.querySelector('#round-playarea').checked ? `${Math.round(effectiveWidth * 2.54)}cm x ${Math.round(effectiveHeight * 2.54)}cm` : `${effectiveWidth * 2.54}cm x ${effectiveHeight * 2.54}cm`;
     document.getElementById('playarea-mm').innerHTML = document.querySelector('#round-playarea').checked ? `${Math.round(effectiveWidth * 25.4)}mm x ${Math.round(effectiveHeight * 25.4)}mm` : `${effectiveWidth * 25.4}mm x ${effectiveHeight * 25.4}mm`;
 }
+*/
 
 window.addEventListener('load', () => {
 
